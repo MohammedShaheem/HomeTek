ecc6e952d3dfa603cddc98bce11a59b9632768e5
Mohammed Shaheem TK <m.shaheem@nestotechnologies.com>
2026-07-21 11:14:45 +0530

    fix->in home page hovering on top products

--- apps/home/views.py
+++ apps/home/views.py
@@ -9,7 +9,7 @@ from django.template.loader import render_to_string
 
 
 class HomeView(TemplateView):
-    template_name = 'nhome.html'
+    template_name = 'home.html'
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
